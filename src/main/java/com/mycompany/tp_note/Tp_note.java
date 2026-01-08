372ea614693c0dcd9b6fa1afeb6f8546c0b73997
JoeNajib02 <joenajibb@gmail.com>
2026-01-08 16:55:01 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/tp_note/Tp_note.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tp_note;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Tp_note {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
