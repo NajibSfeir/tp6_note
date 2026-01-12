8d47ac4bfbb99ff3819ef6a046fb79d434bb7d42
michel <michel.sfeir@eleves.ec-nantes.fr>
2026-01-12 15:36:11 +0100

    added GameController.java w/o errors

---
+++ src/main/java/com/mycompany/tp_note/engine/GameController.java
@@ -0,0 +1,36 @@
+package com.mycompany.tp_note.engine;
+
+import com.mycompany.tp_note.engine.GameState.Status;
+import com.mycompany.tp_note.UI.UserInterface;
+
+/**
+ * Orchestrates the game flow.
+ */
+public class GameController {
+
+    private final HangmanGame game;
+    private final UserInterface ui;
+
+    /**
+     * Initializes the controller with the game logic and user interface.
+     * 
+     * @param game The game engine.
+     * @param ui   The user interface.
+     */
+    public GameController(HangmanGame game, UserInterface ui) {
+        this.game = game;
+        this.ui = ui;
+    }
+
+    /**
+     * Starts and manages the game loop.
+     */
+    public void startGame() {
+        while (game.getState().getCurrentStatus() == Status.PLAYING) {
+            ui.displayGameState(game.getState());
+            char letter = ui.askForLetter();
+            game.guessLetter(letter);
+        }
+        ui.displayEndGame(game.getState());
+    }
+}
